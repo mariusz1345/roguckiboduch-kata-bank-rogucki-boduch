30a9b4c259f7109bebfd222292cdabc58ec11a55
Janek <janekb1996@gmail.com>
2018-05-16 23:54:47 +0200

    aktualizacja given i then w InterestRate

--- src/test/groovy/com/plgrnds/tests/bank/steps/InterestRate.groovy
+++ src/test/groovy/com/plgrnds/tests/bank/steps/InterestRate.groovy
@@ -22,7 +22,8 @@ this.metaClass.mixin(cucumber.api.groovy.EN)
 @Field Set<Deposit> customerDeposits
 
 Given(~/a customer has a new deposit for a period of 6 months with funds 100^$/) { ->
-
+	customer = new Customer()
+	deposit = new Deposit(customer, 90)
 }
 
 And(~/the deposit yearly interest rate is 10%/) { ->
@@ -35,7 +36,7 @@ When(~/a termination date has passed/) { ->
 }
 
 Then(~/105^$ is transferred back to his account/) { ->
-
+	firstAccount.balance + 105.00
 }
 
 
