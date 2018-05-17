1201e5ad0c4e93de741149205e48d2234d2a1da6
Mariusz <mariusz16@op.pl>
2018-05-17 17:07:00 +0200

    Dodanie obslugi odsetek w klasie Deposit, uzupelnienie scenariusza Interest Rate. Dodanie obsugi dat w klasie Deposit.

--- src/main/java/com/plgrnds/tests/bank/Deposit.java
+++ src/main/java/com/plgrnds/tests/bank/Deposit.java
@@ -9,6 +9,10 @@ public class Deposit {
     private double depositBalance;
     private Calendar openTime;
     private Calendar terminationTime;
+    public Calendar now = Calendar.getInstance();
+    private double interestRate;
+    private int howManyMonthsWillDepositLast;
+
 
 
 
@@ -17,8 +21,10 @@ public class Deposit {
         this.setDepositBalance(depositBalance);
         this.openTime = Calendar.getInstance();
     }
+
+
     public double getDepositBalance() {
-        return depositBalance;
+        return depositBalance + (depositBalance*interestRate/100)*howManyMonthsWillDepositLast/12;
     }
 
     public void setDepositBalance(double depositBalance) {
@@ -28,10 +34,26 @@ public class Deposit {
         return openTime.getTime();
     }
 
-    public void setTerminationTimeByYears (int years) {
-        openTime.add(Calendar.YEAR, years);
+    public void setTerminationTimeByMonths (int months) {
+        openTime.add(Calendar.MONTH, months);
     }
     public Calendar getTerminationTime() {
         return terminationTime;
+    }
+    public double getInterestRate() {
+        return interestRate;
+    }
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+
+    public int getHowManyMonthsWillDepositLast() {
+        return howManyMonthsWillDepositLast;
+    }
+
+
+    public void setHowManyMonthsWillDepositLast(int howManyMonthsWillDepositLast) {
+        this.howManyMonthsWillDepositLast = howManyMonthsWillDepositLast;
     }
 }
