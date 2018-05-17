b8a2ba0892553381ecc69a6a1670180529466c88
Mariusz <mariusz16@op.pl>
2018-05-17 16:22:01 +0200

    Uzupelnienie scenariuszu TerminationDate, uzupelnienie klasy Deposit o obsluge dat.

--- src/main/java/com/plgrnds/tests/bank/Deposit.java
+++ src/main/java/com/plgrnds/tests/bank/Deposit.java
@@ -1,15 +1,21 @@
 package com.plgrnds.tests.bank;
 
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class Deposit {
     private Customer owner;
     private double depositBalance;
+    private Calendar openTime;
+    private Calendar terminationTime;
 
 
 
     public Deposit(Customer owner, double depositBalance) {
         this.owner=owner;
         this.setDepositBalance(depositBalance);
-
+        this.openTime = Calendar.getInstance();
     }
     public double getDepositBalance() {
         return depositBalance;
@@ -17,5 +23,15 @@ public class Deposit {
 
     public void setDepositBalance(double depositBalance) {
         this.depositBalance = depositBalance;
+    }
+    public Date getTime() {
+        return openTime.getTime();
+    }
+
+    public void setTerminationTimeByYears (int years) {
+        openTime.add(Calendar.YEAR, years);
+    }
+    public Calendar getTerminationTime() {
+        return terminationTime;
     }
 }
--- src/test/groovy/com/plgrnds/tests/bank/steps/TerminationDate.groovy
+++ src/test/groovy/com/plgrnds/tests/bank/steps/TerminationDate.groovy
@@ -25,21 +25,29 @@ this.metaClass.mixin(cucumber.api.groovy.EN)
 @Field Bank bank
 
 @Field Deposit deposit
+@Field Calendar now
 
-@Field firstAccount
+@Field Account firstAccount
 @Field Set<Account> customerAccounts
 @Field Set<Deposit> customerDeposits
 
 Given(~/a customer opened a deposit for a period of one year/) { ->
-
+    accountRepository = new InMemoryAccountRepository()
+    bank = new Bank(accountRepository)
+    customer = new Customer()
+    firstAccount = new Account(customer, 0)
+    accountRepository.addAccount(firstAccount)
+    deposit = new Deposit(customer, 0)
+    deposit.setTerminationDateByYears(1)
 }
 
 When(~/one year has passed/) { ->
-
+    now==deposit.getTerminationTime
 }
 
 Then(~/the money is transferred back to the account the funds were taken from/) { ->
-
+    firstAccount.balance = firstAccount.balance + deposit.depositBalance
+    deposit.depositBalance = 0
 }
 
 
