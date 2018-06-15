ce66d6703fa98f83e48938cef1473f394672ad5e
Mariusz <mariusz16@op.pl>
2018-06-15 08:38:22 +0200

    Dodanie zarysu nowego scenariusza 2 podpunkty- Deposit Insurance - Cost

---
+++ src/test/groovy/com/plgrnds/tests/bank/steps/DepositInsuranceCost.groovy
@@ -0,0 +1,44 @@
+package com.plgrnds.tests.bank.steps
+
+package com.plgrnds.tests.bank.steps
+
+import com.plgrnds.tests.bank.Account
+import com.plgrnds.tests.bank.AccountRepository
+import com.plgrnds.tests.bank.Bank
+import com.plgrnds.tests.bank.Customer
+import com.plgrnds.tests.bank.InMemoryAccountRepository
+import groovy.transform.Field
+
+import static org.assertj.core.api.Assertions.assertThat
+
+this.metaClass.mixin(cucumber.api.groovy.EN)
+
+@Field AccountRepository accountRepository
+@Field Customer customer
+@Field Bank bank
+
+@Field Set<Account> customerAccounts
+@Field newFunds
+@Field interestRateForNewFunds
+@Field interestForNewFunds
+@Field howManyMonthsWillDepositLast
+
+Given(~/^$/) { ->
+    customer = new Customer()
+    accountRepository.addAccount(firstAccount)
+    deposit = new Deposit(customer, 100)
+
+}
+
+Given(~/^he transfers new funds to the existing deposit$/) { ->
+    deposit.depositBalance = deposit.depositBalance + newFunds
+}
+
+When(~/^the interest rate for these funds is 0.5% greater than the original interest rate$/) { ->
+    interestRateForNewFunds = deposit.interestRate + 0.5
+}
+
+Then(~/^the interest for this funds is proportional to the deposit time left$/) { ->
+    interestForNewFunds = (interestRateForNewFunds * newFunds) * howManyMonthsWillDepositLast
+}
+
--- src/test/groovy/com/plgrnds/tests/bank/steps/InterestRate.groovy
+++ src/test/groovy/com/plgrnds/tests/bank/steps/InterestRate.groovy
@@ -13,32 +13,34 @@ this.metaClass.mixin(cucumber.api.groovy.EN)
 
 @Field AccountRepository accountRepository
 @Field Customer customer
-@Field Bank bank
+
 
 @Field Deposit deposit
 
 @Field Calendar now
 
-@Field firstAccount
+
 @Field Set<Account> customerAccounts
 @Field Set<Deposit> customerDeposits
 @Field interestRate
+@Field insurance
 
-Given(~/a customer has a new deposit for a period of 6 months with funds 100^$/) { ->
+Given(~/there is a customer who is about to open a new deposit of any kind^$/) { ->
 	customer = new Customer()
 	deposit = new Deposit(customer, 100)
 }
 
-And(~/the deposit yearly interest rate is 10%/) { ->
-	deposit.interestRate = 10
+And(~/he decided to add the insurance to the deposit^$/) { ->
+	insurance = new Insurance()
+
 }
 
 
 When(~/a termination date has passed/) { ->
-	now==deposit.getTermiantionTime
+
 }
 
 Then(~/105^$ is transferred back to his account/) { ->
-	firstAccount.balance + 105.00
+
 }
 
